66deeae67d4ef3c8152698f1762f769e7e89bb56
Andi Achmad <emailanda@gmail.com>
2026-07-02 00:45:19 +0800

    fourth update: insatall jwt and env and config

--- src/config/db.js
+++ src/config/db.js
@@ -9,4 +9,6 @@ export const pool = new Pool({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD || undefined,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN
 });
