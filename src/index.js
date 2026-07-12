139327daa3582e870aaf5d1d1f219709e22c156a
Andi Achmad <andifadly2004@gmail.com>
2026-07-12 22:47:52 +0800

    nine update: integrasi cross

--- src/index.js
+++ src/index.js
@@ -28,22 +28,31 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
-import customerRoutes from "./routes/cutomerRoutes.js"
-import productRoutes from "./routes/productRoutes.js"
-import SupplierRoutes from "./routes/supplierRoutes.js"
-import authRoutes from "./routes/authRoutes.js"
-import salesRoutes from "./routes/salesRoutes.js"
-import reporRoutes from "./routes/reportRoutes.js"
-import purchasesRoutes from "./routes/purchasesRoutes.js"
+import customerRoutes from "./routes/cutomerRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import SupplierRoutes from "./routes/supplierRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import salesRoutes from "./routes/salesRoutes.js";
+import reporRoutes from "./routes/reportRoutes.js";
+import purchasesRoutes from "./routes/purchasesRoutes.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "./doc/swagger.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.json({
     message: "Selamat datang di API TokoATK",
@@ -53,22 +62,22 @@ app.get("/", (req, res) => {
       categories: "/categories",
       customers: "/customers",
       products: "/products",
-      suppliers:"/suppliers",
+      suppliers: "/suppliers",
       sales: "/sales",
       repor: "/report",
-      purchases: "/purchases"
+      purchases: "/purchases",
     },
   });
 });
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/categories", categoryRoutes);
-app.use('/customers', customerRoutes);
-app.use('/products', productRoutes);
-app.use('/suppliers', SupplierRoutes);
-app.use('/sales', salesRoutes);
-app.use('/report', reporRoutes);
-app.use('/purchases', purchasesRoutes);
+app.use("/customers", customerRoutes);
+app.use("/products", productRoutes);
+app.use("/suppliers", SupplierRoutes);
+app.use("/sales", salesRoutes);
+app.use("/report", reporRoutes);
+app.use("/purchases", purchasesRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
